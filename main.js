227079b58ef1e3f6c85221915229e3d2f964f422
5H4N3O <shanepratt2002@gmail.com>
2025-08-13 22:18:53 -0500

    replace inline HTML event handlers in index.html with event listeners in main.js

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ const tipSelection = document.getElementById("tip-selection");
 const splitInput = document.getElementById("split-input");
 const calculateBtn = document.getElementById("calculate-button");
 const output = document.getElementById("output");
+
+// Reveal or hide the custom tip percentage input based on the user's selection
+tipSelection.addEventListener("change", () => {
+  updateCustomInputDisplay(tipSelection, customTipInput);
+});
+
+// Calculate tip when button is clicked
+calculateBtn.addEventListener("click", () => {
+  // Get current user inputs (Make seperate function)
+  const billAmount = billInput.valueAsNumber;
+  const tipPercentage = handleTipInput(tipSelection, customTipInput);
+  const splitBetween = splitInput.valueAsNumber;
+  // Calculate and display results
+  const data = calculateTip(billAmount, tipPercentage, splitBetween, output);
+  console.log(data);
+
+  outputData(data, output);
+});
