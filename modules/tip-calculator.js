624875acc5e27c436da49a29556aecbc0bf3e909
5H4N3O <shanepratt2002@gmail.com>
2025-08-14 22:05:21 -0500

    Update tip-calculator.js

--- modules/tip-calculator.js
+++ modules/tip-calculator.js
@@ -1,17 +1,12 @@
 export function handleTipInput(selectedTipOption, customTipInput) {
   // Extract tip percentage
-  const tipInput = (
+  const tipInput =
     selectedTipOption.value === "C"
       ? customTipInput.value
-      : selectedTipOption.value
-  );
+      : selectedTipOption.value;
   return normalizeTipInput(tipInput);
 }
 
-export function updateCustomInputDisplay(selectedTipOption, customTipInput){
-  // Update the custom input display
-  customTipInput.style.display =
-    selectedTipOption.value === "C" ? "inline" : "none";
 export function updateCustomInputDisplay(selectedTipOption, customTipInput) {
     if (selectedTipOption.value === "C") {
       customTipInput.classList.add("show");
@@ -31,59 +26,73 @@ export function calculateTip(billAmount, tipPercentage, splitBetween, output) {
     tipAmount: tipAmount,
     splitTip: splitTip,
     splitBetween: splitBetween,
-    totalAmount: totalAmount
+    totalAmount: totalAmount,
   };
 
   return processedData;
 }
 
 export function outputData(data, output) {
+  // Toggle between error and show
+  output.classList.remove("error");
+  output.classList.add("show");
+  if (data.splitBetween > 1){
   output.innerHTML = `
-    <p>This is a test of outputting the calculated tip<p>
-    <p>Bill: $${data.billAmount.toFixed(2)}<p>
-    <p>Tip(${data.tipPercentage.toFixed(2)}) amount (${data.tipAmount.toFixed(2)}) split between
-    ${data.splitBetween} people: $${data.splitTip.toFixed(2)}<p>
-    <p>Total: $${data.totalAmount.toFixed(2)}<p>
+    <p>Tip Amount: $${data.tipAmount.toFixed(2)}</p>
+    <p>Total: $${data.totalAmount.toFixed(2)}</p>
+    <p>Splitting tip between ${
+      data.splitBetween
+    } people...</p>
+    <p>Tip per person: $${data.splitTip.toFixed(2)}</p>
     `;
+  } else {
+    output.innerHTML = `
+    <p>Tip Amount: $${data.tipAmount.toFixed(2)}</p>
+    <p>Total: $${data.totalAmount.toFixed(2)}</p>
+    `;
+  }
 }
 
 export function outputErrors(errors, output) {
-  //output.style.display = "block";
+  // Toggle between error and show
+  output.classList.remove("show");
+  output.classList.add("error");
   output.innerHTML = `
-    <ul>
-      ${errors.map((err) => `<li>${err}</li>`).join("")}
-    </ul>
+    
+    ${errors.map((err) => `<p>${err}<p>`).join("")}
+    
   `;
 }
 
 export function normalizeTipInput(input) {
-  if (typeof input == 'string') {
+  if (typeof input == "string") {
     input = input.trim(); //remove any whitespace
 
-    if (input.endsWith('%')) {
-      input = input.slice(0,-1); // Remove percent sign if user input, say, 25%
+    if (input.endsWith("%")) {
+      input = input.slice(0, -1); // Remove percent sign if user input, say, 25%
     }
   }
 
   let num = parseFloat(input);
 
   // If input still didnt convert to number
-  if (isNaN(num)) return 0;
+  if (isNaN(num) || num <= 0) return 0;
 
   // If user input their percentage as whole percent (input: 25 -> 0.25)
-  if (num > 1) {
-    num = num / 100;
-  }
+  // if (num >= 1) {
+  //   num = num / 100;
+  // }
 
-  return num;
+  return (num/100);
 }
 
 export function validateInputs(billAmount, tipPercentage, splitBetween) {
   const errors = [];
 
   if (billAmount <= 0) errors.push("Bill amount must be greater than 0");
-  if (tipPercentage < 0 || tipPercentage > 1)
-    errors.push("Tip percentage must be between 0 and 1");
+  if (isNaN(billAmount)) errors.push("Please enter a bill amount");
+  if (tipPercentage <= 0 || tipPercentage > 1)
+    errors.push("Tip % must be between 0 and 100");
   if (splitBetween < 1) errors.push("Split must be at least 1");
 
   return errors;
